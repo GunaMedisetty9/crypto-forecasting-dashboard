f7c7106b95651ba8f10f13b4e150c5dfcbed9564
Guna Medisetty <128883236+GunaMedisetty9@users.noreply.github.com>
2026-01-15 07:47:27 +0530

    Update crypto_dashboard_v2.py

--- crypto_dashboard_v2.py
+++ crypto_dashboard_v2.py
@@ -50,6 +50,9 @@ if st.session_state.theme == 'dark':
         .stApp {
     background: linear-gradient(180deg, #0e1117 0%, #1a1d29 100%) !important;  ###here is where black satisfied just dashboard but not sidebar
 }
+section[data-testid="stSidebar"] section {
+    background: linear-gradient(180deg, #1e2130 0%, #2a2d3a 100%) !important;
+}
 [data-testid="stHeader"] {
     position: relative !important;
     background: linear-gradient(180deg, #FFF8DC 0%, #FFE4B5 100%) !important;    ###here is where Yellow top satisfied just dont trychanging this 
